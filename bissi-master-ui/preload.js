cff6b3ad2884918aacfd791281983e6109b56111
goldensam777 <samuelyevidev777@gmail.com>
2026-05-07 09:17:20 +0100

    Update UI: chat.html and preload.js design changes

--- bissi-master-ui/preload.js
+++ bissi-master-ui/preload.js
@@ -1,25 +1,22 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
-// Expose secure API to renderer process
 contextBridge.exposeInMainWorld('bissi', {
-  // Profile operations
   profile: {
     isFirst: () => ipcRenderer.invoke('profile:isFirst'),
     save: (firstName, lastName) => ipcRenderer.invoke('profile:save', firstName, lastName),
     load: () => ipcRenderer.invoke('profile:load')
   },
-  
-  // Navigation
   nav: {
     goToChat: () => ipcRenderer.invoke('nav:goToChat')
   },
-  
-  // Models and backend
   models: {
     list: () => ipcRenderer.invoke('models:list')
   },
-  
   ollama: {
     models: () => ipcRenderer.invoke('ollama:models')
+  },
+  file: {
+    read: (filePath) => ipcRenderer.invoke('file:read', filePath),
+    readBuffer: (filePath) => ipcRenderer.invoke('file:readBuffer', filePath)
   }
 });
